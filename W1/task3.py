8117b121dbe8583d28d38af8229e2e2f9bc4f82e
Adweta <sadweta22@tbc.edu.np>
2023-11-08 14:56:30 +0545

    Week1

--- W1/task3.py
+++ W1/task3.py
@@ -1,2 +1,17 @@
 add = 45 + 20
 print(add)
+
+exp_1 = 10 + 20 - 15
+print(exp_1)
+
+exp_2 = 10 * 5
+print(exp_2)
+
+exp_3 = 100 / 33
+print(exp_3)
+
+exp_4 = 10 ** 2
+print(exp_4)
+
+exp_5 = 10 % 3
+print(exp_5)
