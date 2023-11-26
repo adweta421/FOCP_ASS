bd871063d516d0d3a6f086ef942aa6e0f49210d6
adweta421 <126636157+adweta421@users.noreply.github.com>
2023-11-26 10:22:44 +0545

    Week 2

---
+++ Week2/task17.py
@@ -0,0 +1,25 @@
+# Defination
+
+Assignment:
+ Assignment is the process of giving a value to a variable. In programming languages like Python, you use the assignment operator (=) to bind a value to a variable.
+
+
+Data-type:
+Data type is a classification that specifies which type of value a variable can hold. In Python, common data types include integers, floats, strings, lists, and more.
+
+
+Argument:
+ An argument is a value that you pass to a function or a method when calling it. It is a way to provide input to a function or method for it to process.
+
+
+Indexing:
+Indexing refers to the process of accessing individual elements in a data structure, like a list or a string, using their position or index. In Python, indexing starts at 0.
+
+Slicing:
+Slicing is a way to extract a portion of a sequence, such as a list or a string. It is done by specifying a range of indices, and it returns a new sequence containing the elements within that range.
+
+Mutable:
+Mutable objects are those whose values or content can be changed after they are created. Lists in Python are an example of mutable objects. You can modify, add, or remove elements in a mutable object.
+
+Immutable:
+Immutable objects, on the other hand, cannot be changed once they are created. Examples of immutable objects in Python include strings and tuples. Once created, their values cannot be modified.
