a1258b6497f43af308eec815a6d47e4d0f03d786
adweta421 <126636157+adweta421@users.noreply.github.com>
2023-12-27 10:02:05 +0545

    Week 5

---
+++ Week5/first_prog.py
@@ -0,0 +1,10 @@
+number = input("Enter a number: ")
+
+number = int(number)
+
+print("The numbered entered was", number)
+
+if (number % 2) == 0:
+	print("That is an even number")
+else:
+	print("That is an odd number")
