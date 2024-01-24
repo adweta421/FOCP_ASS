cbf8645845b594a7c46bd045292524cc5d24f4fe
adweta421 <126636157+adweta421@users.noreply.github.com>
2024-01-24 21:16:35 +0545

    Portfolio

--- Portfolio/samp.py
+++
@@ -1,9 +0,0 @@
-if 0 <= number <= 12:
-    print(f"Multiplication table for {number}:")
-
-    # Calculate and print the table
-    for i in range(1, 11):
-        result = number * i
-        print(f"{number} x {i} = {result}")
-else:
-    print("Please enter a valid number between 0 and 12.")
--- Portfolio/sampl.py
+++
@@ -1,5 +0,0 @@
-num = int(input("Enter a value:"))
-if num % 2 == 0:
-    print ("even")
-else:
-    print("odd")
